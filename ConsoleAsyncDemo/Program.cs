ee6870e8a4c7643446a64511ed0085a952bea15b
Daniel Córdoba <daniel.cordoba@prodware.es>
2018-11-06 10:36:29 +0100

    Add project files.

---
+++ ConsoleAsyncDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleAsyncDemo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Main().Wait();
+        }
+
+        static private async Task Main()
+        {
+            //do async stuff
+        }
+    }
+}
